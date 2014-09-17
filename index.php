8f78ef68a7c6bad265ef02c8596707a145819b05
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-09-17 08:19:55 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Portal Home</title>
+		<link rel="stylesheet" type="text/css" href="styles.css" title="Default Styles" media="screen"/>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans" title="Font Styles"/>
+	</head>
+	
+	<body>
+		<div class="container" style="background-color:#f4f4f4;width:70%;">
+			
+			<article>
+				<p>
+					<!-- <center><img src="logo_big.png"></center> Insert Main Logo here -->
+						
+						<h2> <a href="http://myriadplatform.org" target="_blank"><img src="http://birdspool.no-ip.org:5567/static/img/logo.png" style="width:12%;"</img>  Myriadcoin </a></h2>
+        <p>
+	<strong>Myriadcoin</strong> is the first multi-PoW cryptocurrency that uses 5 concurrent algorithms, each of which have an equal chance of solving the next block. 
+Each algorithm has an independent difficulty, and a block time of 2.5 minutes, which averages out to 30 seconds per block across all the algorithms. 
+The 5 algorithms that comprise Myriadcoin are SHA256, Scrypt, Groestl, Skein, and Qubit. 
+For more information, visit <a href="http://myriadplatform.org" target="_blank">MyriadPlatform.org</a></p>
+<br/>
+	<p>Links of interest: 
+	<ul>
+		<li><a href="http://birdonwheels5.no-ip.org/richlist/"target="_self">Top 1000 Richest Myriadcoin Addresses</a>
+		<li><a href="http://birdonwheels5.no-ip.org/myriad-bounty/" target="_self">Myriadcoin Bounty Board</a>
+		<li><a href="http://birdonwheels5.no-ip.org/myr-hub/" target="_self">Bird's Myriadcoin P2pool Hub</a>
+		<li><a href="http://birdonwheels5.no-ip.org/home/" target="_self">Bird's Website Home Page</a>
+	</ul></p>
+
+        <p><h3>Algorithm Statistics:</h3> (refresh the page to refresh the values)<br/>
+	  <table class="table" data-ng-controller="StatusController" data-ng-init="getStatus('Info')">	
+	    <tr>
+		<td>Algorithm: </td>
+		<td>SHA256</td>
+		<td>Scrypt</td>
+		<td>Skein</td>
+		<td>Groestl</td>
+		<td>Qubit</td>
+	    </tr>
+	    <tr>
+		<td>Difficulty:</td>
+		<td id="sha">{{info.difficulty_sha256d}}</td>
+		<td>{{info.difficulty_scrypt}}</td>
+		<td>{{info.difficulty_skein}}</td>
+		<td>{{info.difficulty_groestl}}</td>
+		<td>{{info.difficulty_qubit}}</td>
+	    </tr>
+	    <tr>
+		<td>Network Hashrate:</td>
+		<td id="sha_net">N/A</td>
+		<td>N/A</td>
+		<td>N/A</td>
+		<td>N/A</td>
+		<td>N/A</td>
+	    </tr>
+	    <tr>
+		<td>Profitability per Scrypt MH/s:</td>
+		<td>N/A</td>
+		<td>N/A</td>
+		<td>N/A</td>
+		<td>N/A</td>
+		<td>N/A</td>
+	    </tr>
+	    <tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	    </tr>
+
+</p>
+
+				</p>
+			
+			
+			</article>
+			
+			<div class="paddingBottom">
+			</div>
+		</div>
+	</body>
+	
+</html>
