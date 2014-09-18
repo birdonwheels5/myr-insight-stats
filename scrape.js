e84afc528838034b11bae65e48da7a9f08af4b73
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-09-18 11:27:53 -0400

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,17 +1,23 @@
 var args = require('system').args;
 var address = '';
+var fileName = '';
 args.forEach(function(arg, i) {
   
   if(i == 1)
   {
     address = arg;
   }
+  
+  if(i == 2)
+  {
+    fileName = arg;
+  }
 });
 
 
 var page = require('webpage').create();
 var fs = require('fs');// File System Module
-var output = '/var/www/myr-insight-stats/scrape.html'; // path for saving the local file 
+var output = '/var/www/myr-insight-stats/scrapes/' + fileName; // path for saving the local file 
 page.open(address, function() { // open the file 
 fs.write(output,page.content,'w'); // Write the page to the local file using page.content
 phantom.exit(); // exit PhantomJs
