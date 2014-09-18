106aae5896d41628291f2760132263a3d4008792
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-09-17 21:15:12 -0400

    Update methodHelper.php

--- methodHelper.php
+++ methodHelper.php
@@ -51,7 +51,9 @@ function getDifficulty($algorithm)
   	// Break the data up into an array
   	$explodedString = explode("ng-binding\">", $fullString);
   	
-  	var_dump($explodedString);
+  	//var_dump($explodedString);
+  	
+  	print $explodedString[26] . "\n" . $explodedString[27] . "\n" . $explodedString[28] . "\n" . $explodedString[29] . "\n" . $explodedString[30] . "\n" . $explodedString[31]
   	
   	// Clean it up (it will always be the 47th position in the array)
   	//$difficulty = str_ireplace(" id=\"block_difficulty\">", "", $explodedString[47]);
