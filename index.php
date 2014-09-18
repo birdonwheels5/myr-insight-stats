05ad545ce527428a2573bd1ce88d089a8ffd5a71
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-09-17 22:22:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -73,11 +73,11 @@ For more information, visit <a href="http://myriadplatform.org" target="_blank">
 	    </tr>
 	    <tr>
 		<td>Network Hashrate:</td>
-		<td><?php print $sha_hashrate; ?> TH/S</td>
-		<td><?php print $scrypt_hashrate; ?> GH/s</td>
-		<td><?php print $skein_hashrate; ?> GH/s</td>
-		<td><?php print $groestl_hashrate; ?> GH/s</td>
-		<td><?php print $qubit_hashrate; ?> $nbsp GH/s</td>
+		<td><?php print $sha_hashrate; ?> &nbsp TH/S</td>
+		<td><?php print $scrypt_hashrate; ?> &nbsp GH/s</td>
+		<td><?php print $skein_hashrate; ?> &nbsp GH/s</td>
+		<td><?php print $groestl_hashrate; ?> &nbsp GH/s</td>
+		<td><?php print $qubit_hashrate; ?> &nbsp GH/s</td>
 	    </tr>
 	    <tr>
 		<td>Profitability per Scrypt MH/s:</td>
