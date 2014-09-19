54e41af174bcacdab0089b133be37858d93689e2
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-09-19 14:43:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,17 @@
 	
 	<?php
 		
+		// These values are used to compute coins/day for all algos. 
+		// Measured in MH/s and are estimates of 1 Scrypt MH/s
+		$sha_hash = 1000;
+		$scrypt_hash = 1;
+		$skein_hash = 325;
+		$groestl_hash = 18;
+		$qubit_hash = 8;
+		
+		$hash_multiplier = 1000000;
+		$coins_per_block = 1000;
+		
 		scrape("http://birdonwheels5.no-ip.org:3000/status", "scrape.html");
 		
 		$diff = array();
@@ -20,18 +31,24 @@
 		
 		$sha_diff = number_format($diff[0], 2, '.', ',');
 		$sha_hashrate = number_format(($diff[0]/34.92331797)/1000, 2, '.', ',');
+		// 		  Units:     Sec	Diff	   ???	   hashrate MH/s
+		$sha_profit = number_format((86400 / (($diff[0] * 2**32) / $sha_hash * $hash_multiplier)) * $coins_per_block, 2, '.', ',');
 		
 		$scrypt_diff = number_format($diff[1], 2, '.', ',');
 		$scrypt_hashrate = number_format($diff[1]/34.92331797, 2, '.', ',');
+		$scrypt_profit = number_format((86400 / (($diff[1] * 2**32) / $scrypt_hash * $hash_multiplier)) * $coins_per_block, 2, '.', ',');
 		
 		$skein_diff = number_format($diff[2], 2, '.', ',');
 		$skein_hashrate = number_format($diff[2]/34.92331797, 2, '.', ',');
+		$skein_profit = number_format((86400 / (($diff[2] * 2**32) / $skein_hash * $hash_multiplier)) * $coins_per_block, 2, '.', ',');
 		
 		$groestl_diff = number_format($diff[3], 2, '.', ',');
 		$groestl_hashrate = number_format($diff[3]/34.92331797, 2, '.', ',');
+		$groestl_profit = number_format((86400 / (($diff[3] * 2**32) / $groestl_hash * $hash_multiplier)) * $coins_per_block, 2, '.', ',');
 		
 		$qubit_diff = number_format($diff[4], 2, '.', ',');
 		$qubit_hashrate = number_format($diff[4]/34.92331797, 2, '.', ',');
+		$qubit_profit = number_format((86400 / (($diff[4] * 2**32) / $qubit_hash * $hash_multiplier)) * $coins_per_block, 2, '.', ',');
 	?>
 	
 		<div class="container" style="width:100%;float:left;">
